b668ead925e7f1c853907a3edc2647b687adb4f0
Carolina Pinto <carolinaop.co2003@gmail.com>
2026-03-06 10:12:29 +0000

    Bootstrap installation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  const [data, setData] = useState("");
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(0);
 
   useEffect(() => {
     fetch("http://127.0.0.1:8000/api/test")
@@ -10,10 +15,26 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1>React + FastAPI</h1>
+    <>
+      <div>
+        <Header></Header>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
       <p>{data}</p>
-    </div>
+    </>
   );
 }
 
